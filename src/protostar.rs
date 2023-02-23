46f2a47ce4aa1bc0fcb697e88a4d5d216ec3ab1d
nik012003 <guerrera.nicola@gmail.com>
2023-02-23 13:49:29 +0100

    Added texture to hexagons

--- src/protostar.rs
+++ src/protostar.rs
@@ -36,7 +36,7 @@ fn model_from_icon(parent: &Spatial, icon: &Icon) -> Result<Model> {
 				MaterialParameter::Color([0.0,1.0,1.0,1.0]),
 			)?;
 			model.set_material_parameter(
-				2,
+				0,
 				"diffuse",
 				MaterialParameter::Texture(ResourceID::Direct(icon.path.clone())),
 			)?;
